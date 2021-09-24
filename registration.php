eff5877b68fe160330a1fcbe2d6e1904925b7eae
sickdaflip <philippbreitsprecher@gmail.com>
2021-09-24 11:10:06 +0200

    first commit

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/FlipDev/theme-frontend-foundation',
+    __DIR__
+);
